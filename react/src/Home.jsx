04a5c5c1906ca030f9241ef4fde80d7df16cafdf
akshit-sharma-15 <asharma889452@gmail.com>
2025-03-07 11:59:56 +0530

    done

---
+++ react/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h1 className="text-2xl font-bold">Akshit sharma</h1>
+            <p className="mt-2">Contact: akshitbhai15@gmail.com</p>
+            <p>Phone: +91 8894521XXX</p>
+        </div>
+    );
+};
+
+export default Home;
